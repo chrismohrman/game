18d4af37a6763fe1689d2d1f64031fb7ab7f484d
chrismohrman <37715827+chrismohrman@users.noreply.github.com>
2018-07-13 16:54:29 -0400

    Fixed Orc Sprite

--- systems/orc-update-sprite.lua
+++ systems/orc-update-sprite.lua
@@ -1,15 +1,26 @@
 return function(entity, dt)
   local vel_x, vel_y = entity.body:getLinearVelocity()
 
-  if vel_x == 0 and vel_y == 0 then entity.active_sprite = entity.sprites.stand_down
+  print(vel_x, vel_y)
+
+  if vel_x == 0 and vel_y == 0 then
+    entity.active_sprite = entity.sprites.stand_down
   end
 
-  if vel_x > 0 then entity.active_sprite = entity.sprites.move_right
+  if vel_x > 0 then
+    entity.active_sprite = entity.sprites.move_right
+    print('right')
   end
-  if vel_x < 0 then entity.active_sprite = entity.sprites.move_left
+  if vel_x < 0 then
+    entity.active_sprite = entity.sprites.move_left
+    print('left')
   end
-  if vel_y > 0 then entity.active_sprite = entity.sprites.move_down
+  if vel_y > 0 then
+    entity.active_sprite = entity.sprites.move_down
+    print('down')
   end
-  if vel_y < 0 then entity.active_sprite = entity.sprites.move_up
+  if vel_y < 0 then
+    entity.active_sprite = entity.sprites.move_up
+    print('up')
   end
 end
